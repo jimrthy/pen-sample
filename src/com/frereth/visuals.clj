154b1a34261d99512ceffd7fe0cb645fc71d59f8
James Gatannah <james.gatannah@gmail.com>
2013-10-16 15:26:14 -0500

    Add a frame counter. Which, realistically, just shows that drawing is only happening in response to events which dirty the window...which, also realistically, is exactly when it should be happening

--- src/com/frereth/visuals.clj
+++ src/com/frereth/visuals.clj
@@ -7,16 +7,21 @@
   (comment (gl/frustum-view 60.0 (/ (double w) h) 1.0 100.0)
            (gl/load-identity)))
 
+(defn update [[delta time] state]
+  (into state {:frame-count (inc (:frame-count state))}))
+
 (defn display [[delta time] state]
-  (gl/translate 0 -0.93 -3)
-  (gl/draw-triangles
-   (gl/color 1 0 0) (gl/vertex 1 0)
-   (gl/color 0 1 0) (gl/vertex -1 0)
-   (gl/color 0 0 1) (gl/vertex 0 1.86)))
+  (let [frame (:frame-count state)]
+    (println "Draw Frame" frame)
+    (gl/translate 0 -0.93 -3)
+    (gl/draw-triangles
+     (gl/color 1 0 0) (gl/vertex 1 0)
+     (gl/color 0 1 0) (gl/vertex -1 0)
+     (gl/color 0 0 1) (gl/vertex 0 1.86))))
 
 (defn -main []
   (app/start
-   {:display display :reshape reshape}
-   {}))
+   {:display display :reshape reshape :update update}
+   {:frame-count 0}))
 
 
